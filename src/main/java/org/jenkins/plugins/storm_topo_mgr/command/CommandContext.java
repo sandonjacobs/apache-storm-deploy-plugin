83962668a89b8485e3d072e0aa1c990ea06d2037
Sandon Jacobs <sjacobs@appia.com>
2016-06-06 15:38:29 -0400

    #2 removing @NotNull annotation, was failing build

--- src/main/java/org/jenkins/plugins/storm_topo_mgr/command/CommandContext.java
+++ src/main/java/org/jenkins/plugins/storm_topo_mgr/command/CommandContext.java
@@ -1,14 +1,12 @@
 package org.jenkins.plugins.storm_topo_mgr.command;
 
-import com.sun.istack.internal.NotNull;
-
 public abstract class CommandContext {
 
     private final StormCommand command;
     private final String nimbusHost;
     private final Integer thriftPort;
 
-    public CommandContext(@NotNull String nimbusHostName, @NotNull Integer nimbusThriftPort, StormCommand cmd) {
+    public CommandContext(String nimbusHostName, Integer nimbusThriftPort, StormCommand cmd) {
         this.command = cmd;
         this.nimbusHost = nimbusHostName;
         this.thriftPort = nimbusThriftPort;
